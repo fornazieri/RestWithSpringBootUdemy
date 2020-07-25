9d425c51fb1750b9c6be3cdeb6151b67e34e4d68
Vitor Fornazieri <viitor.f@hotmail.com>
2020-07-24 21:47:00 -0300

    Adicionando Migrations com Flyway para versionar o banco de dados

---
+++ 07 RestWithSpringBootUdemy Migrations/src/main/java/br/com/vitorfornazieri/restwithspringbootudemy/controller/PersonController.java
@@ -0,0 +1,47 @@
+package br.com.vitorfornazieri.restwithspringbootudemy.controller;
+
+import br.com.vitorfornazieri.restwithspringbootudemy.data.model.Person;
+import br.com.vitorfornazieri.restwithspringbootudemy.data.vo.PersonVO;
+import br.com.vitorfornazieri.restwithspringbootudemy.services.PersonServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+    @Autowired
+    private PersonServices services;
+
+    @GetMapping
+    public List<PersonVO> findAll() {
+        return services.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public PersonVO findById(@PathVariable("id") Long id) {
+        return services.findById(id);
+    }
+
+    @PostMapping
+    public PersonVO create(@RequestBody PersonVO person) {
+        return services.create(person);
+    }
+
+    @PutMapping
+    public PersonVO update(@RequestBody PersonVO person) {
+        return services.update(person);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) { //quando <?> significa que é generico, o que vier ele aceita
+        services.delete(id);
+        //return ResponseEntity.ok("Excluído com sucesso");
+        return ResponseEntity.ok().build();
+    }
+
+}
